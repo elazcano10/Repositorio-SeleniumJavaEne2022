6d8ed82e3b8d555eba45488ca607221231e30716
Erik Lazcano <erik_lazcano@hotmail.com>
2022-02-09 16:37:21 -0600

    Add shouldWakeUp Method Add Barking Main

---
+++ src/test/java/com/java/Barking.java
@@ -0,0 +1,57 @@
+package com.java;
+
+import javax.swing.JOptionPane;
+
+public class Barking {
+
+	//Definición de atributos
+		boolean barking = true;
+		int hourOfDay = 0;
+	
+	//Definición de Métodos
+		
+		
+		
+		
+		public void leerDatos(){
+		
+			hourOfDay = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la hora actual en formato de 24 hrs: "));
+			barking = Boolean.parseBoolean(JOptionPane.showInputDialog("¿El perro está ladrando? (true/false): "));
+			
+		}
+		
+	
+		
+	public void shouldWakeUp(){
+		
+		if (hourOfDay >=0 && hourOfDay<=23)
+		{
+			if(hourOfDay<8 || hourOfDay>22)
+			{	barking=true;
+				System.out.println("El perro está ladrando, debemos despertarnos...");
+				System.out.println("Son las "+hourOfDay +"hrs");
+				System.out.println(barking);
+			}
+			else
+			{
+				barking=false;
+				System.out.println("El perro no está ladrando.");
+				System.out.println("Son las "+hourOfDay +"hrs");
+				System.out.println(barking);
+				
+			}
+			 
+		}
+		else
+		{
+			barking=false;
+			System.out.println("El perro no está ladrando.");
+			System.out.println("Fuera de horario");
+			System.out.println(barking);
+			
+		}
+		
+		
+	}
+	
+}
---
+++ src/test/java/com/java/BarkingMain.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class BarkingMain {
+
+	public static void main(String[] args) {
+	
+		Barking obj = new Barking ();
+		
+		obj.leerDatos();
+		obj.shouldWakeUp();
+
+	}
+
+}
