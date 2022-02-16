457643029f4d4bc57714f5a1031661ab745369ff
Erik Lazcano <erik_lazcano@hotmail.com>
2022-02-15 21:38:49 -0600

    Move to package metodos

---
+++ src/test/java/com/java/metodos/BarkingMain.java
@@ -0,0 +1,14 @@
+package com.java.metodos;
+
+public class BarkingMain {
+
+	public static void main(String[] args) {
+	
+		Barking obj = new Barking ();
+		
+		obj.leerDatos();
+		obj.shouldWakeUp();
+
+	}
+
+}
