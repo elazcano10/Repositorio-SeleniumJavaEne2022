7f3c896c55541df05b82507c61ceccc4b19bc9ad
Erik Lazcano <erik_lazcano@hotmail.com>
2022-02-15 21:39:28 -0600

    Update Decimal

---
+++ src/test/java/com/java/Decimal.java
@@ -0,0 +1,23 @@
+package com.java;
+
+import javax.swing.JOptionPane;
+
+public class Decimal {
+
+	public static void main(String[] args) {
+		
+		int numero1;
+		int numero2;
+		int numero3;
+		int enteroMayor;
+		
+		
+
+		numero1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer número entero: "));
+		numero2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número entero: "));
+		numero3 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el tercer número entero: "));
+		
+
+	}
+
+}
